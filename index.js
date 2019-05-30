b88261b14c2bc45388ce8a0ee351180f606ec6dd
kolkol69 <maksym.kolodiy@netguru.pl>
2019-05-30 10:40:21 +0200

    First js to python example added in indexjs

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const antlr4 = require('antlr4');
-const ECMAScriptLexer = require('./lib/ECMAScriptLexer.js');
-const ECMAScriptParser = require('./lib/ECMAScriptParser.js');
+const antlr4 = require("antlr4");
+const ECMAScriptLexer = require("./lib/ECMAScriptLexer.js");
+const ECMAScriptParser = require("./lib/ECMAScriptParser.js");
+const PythonGenerator = require("./codegeneration/PythonGenerator");
 
-const input = '{x: 1}';
+const input = "{x: 1}";
 
 const chars = new antlr4.InputStream(input);
 const lexer = new ECMAScriptLexer.ECMAScriptLexer(chars);
@@ -11,6 +12,14 @@ lexer.strictMode = false; // do not use js strictMode
 
 const tokens = new antlr4.CommonTokenStream(lexer);
 const parser = new ECMAScriptParser.ECMAScriptParser(tokens);
-const tree = parser.program();
+// const tree = parser.program();
+const tree = parser.expressionSequence();
 
-console.log(tree.toStringTree(parser.ruleNames));
+// console.log(tree.toStringTree(parser.ruleNames));
+console.log("JavaScript input:");
+console.log(input);
+console.log("Python output:");
+
+const output = new PythonGenerator().start(tree);
+
+console.log(output);
